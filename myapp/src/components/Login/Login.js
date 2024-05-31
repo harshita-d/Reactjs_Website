6fc0762839f580868a0fed282c2ace2722ad79de
harshita <hrs.dadhich@gmail.com>
2024-06-01 00:58:55 +0530

    initial commit of login

---
+++ myapp/src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import { ReactComponent as LoginSvg } from "../../assets/images/login.svg";
+
+function Login() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true); // Trigger the slide-in animation when the component mounts
+  }, []);
+  return (
+    <div className="login-div">
+      <div className="svg-login-div">
+        <LoginSvg className="login-svg" />
+      </div>
+      <div className={`login-left-panel ${isVisible ? "animate" : ""}`}>
+        <div className="loginHeading">Login</div>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className="input-div"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className="input-div"
+        />
+        <button type="submit" className="loginSubmit">
+          Login
+        </button>
+        <div className="signin-tab">Signin</div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
